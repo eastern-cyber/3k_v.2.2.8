35206cad5e577707fbd0b960964df891f0b30c30
Eastern Cyber Co., Ltd. <easterncyber.com@gmail.com>
2026-07-24 15:57:53 +0700

    Add django.contrib.sites for social login

--- _core/settings.py
+++ _core/settings.py
@@ -63,6 +63,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites',  # <-- Add this
+
     
     # My apps
     'a_posts',
@@ -82,6 +84,9 @@ INSTALLED_APPS = [
     'allauth.socialaccount.providers.google',
     'allauth.socialaccount.providers.facebook',
 ]
+
+# Add Site ID
+SITE_ID = 1
 
 SOCIALACCOUNT_PROVIDERS = {
     'google': {
