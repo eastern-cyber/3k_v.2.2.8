d47dfa900553c5221e54dffdec2f5b4c9a7c1db6
Eastern Cyber Co., Ltd. <easterncyber.com@gmail.com>
2026-07-25 13:59:10 +0700

    Fix is_following filter for unauthenticated users

--- a_network/templatetags/network_tags.py
+++ a_network/templatetags/network_tags.py
@@ -5,5 +5,27 @@ register = template.Library()
 
 @register.filter
 def is_following(user, this_user):
-    is_following = Follow.objects.filter(follower=user, following=this_user).exists()
-    return is_following
+    """
+    Check if `user` is following `this_user`.
+    Use as: {{ user|is_following:other_user }}
+    Returns False if either user is not authenticated.
+    """
+    # Check if user is authenticated before querying
+    if not user or not user.is_authenticated:
+        return False
+    if not this_user:
+        return False
+    return Follow.objects.filter(follower=user, following=this_user).exists()
+
+@register.simple_tag
+def is_following_tag(user, this_user):
+    """
+    Check if `user` is following `this_user`.
+    Use as: {% is_following_tag user other_user %}
+    Returns False if either user is not authenticated.
+    """
+    if not user or not user.is_authenticated:
+        return False
+    if not this_user:
+        return False
+    return Follow.objects.filter(follower=user, following=this_user).exists()
